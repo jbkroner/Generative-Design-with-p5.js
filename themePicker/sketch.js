63a47f2ae6412ac03f28eb569230a686065b536b
James <jbkroner@gmail.com>
2020-10-30 14:32:57 -0500

    added themePicker

---
+++ themePicker/sketch.js
@@ -0,0 +1,30 @@
+/** help choose themes for our club */
+
+
+function setup(){
+    // creating the canvas and defining some enviroment variables: 
+    HEIGHT = 400;
+    WIDTH = 400;
+    createCanvas(HEIGHT, WIDTH);
+    frameRate(30);
+    backgroundColor = color('#dec0de');
+    counter = 0;
+    creativeThemes = ["colorful", "animated", "deep", "full", "light"];
+    technicalThemes = ["arc", "ellipse", "line", "point", "curve"];
+
+}
+
+
+function draw(){
+    if(counter < 300){
+        ++counter;
+        background(backgroundColor);
+        stroke(0);
+        smooth();
+        text('decode@uwm theme generator', 40, 50);
+        text('technical theme:', 40,150);
+        text(technicalThemes[floor((random() * 5))], 200, 150);
+        text('creative theme:', 40,250);
+        text(creativeThemes[floor((random() * 5))], 200, 250);
+    }
+}
